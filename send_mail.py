d8d60e913d83a467b0adf090484433965e61e4df
Newscred Inc <nure@Newscred-Nure-MBP.local>
2019-10-25 16:52:38 +0600

    Initial Deploy

---
+++ send_mail.py
@@ -0,0 +1,22 @@
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_mail(customer, dealer, rating, comments):
+    port = 2525
+    smtp_server = 'smtp.mailtrap.io'
+    login = 'e891164390c976'
+    password = '0072bb65058495'
+    message = f"<h3>New Feedback Submission</h3><ul><li>Customer: {customer}</li><li>Dealer: {dealer}</li><li>Rating: {rating}</li><li>Comments: {comments}</li></ul>"
+
+    sender_email = 'xyz1@example.com'
+    receiver_email = 'xyz22@example.com'
+    msg = MIMEText(message, 'html')
+    msg['Subject'] = 'BMW Feedback'
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+
+    # Send email
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.login(login, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
